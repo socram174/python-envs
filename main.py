47bf3032ac36b5d39e8e1e7012c6745ab9cb6118
socram174 <socram174@hotmail.com>
2024-07-10 16:11:52 -0400

    ENV test

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ load_dotenv()
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(os.environ["MESSAGE"])
+    if os.environ["MESSAGE"] == "HOLA":
+        print("Si es")
+    else:
+        print("No es igual :(")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
