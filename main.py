8d7077fdb16e97255beb31dfdca5354e06a27dcf
socram174 <socram174@hotmail.com>
2024-07-10 15:47:13 -0400

    ENV test in python

---
+++ main.py
@@ -0,0 +1,10 @@
+from dotenv import  load_dotenv
+import os
+
+load_dotenv()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(os.environ["MESSAGE"])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
